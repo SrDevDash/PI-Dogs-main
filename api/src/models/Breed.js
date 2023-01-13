dff9aa04bff3428fd9b10ad09db8f266143e8e7e
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-13 17:35:18 -0500

    Create Breed and Dog Models

---
+++ api/src/models/Breed.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Breed', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    life_span: {
+      type: DataTypes.STRING,
+    }
+  });
+};
