d351935f8628dfeb230679234fa5d174c0b3ba62
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-13 17:36:12 -0500

    First routing of backend get Dogs and map them

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -8,5 +9,19 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use('/dogs', async (req, res) => {
+    try {
+        const dogs = await axios.get('https://api.thedogapi.com/v1/breeds');
+
+        const mapDogs = dogs.map(dog => {
+            return { id: dog.id, name: dog.name, weight: dog.weight, height: dog.height, life_span: dog.life_span }
+        })
+        res.status(200).send(mapDogs);
+    } catch (error) {
+        res.status(400).send({ error })
+    }
+
+    res.end();
+})
 
 module.exports = router;
