5daf001617485111f1fe026184eab51fdcd77336
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-16 12:05:03 -0500

    adding getByName redux

--- client/src/Redux/Actions/actions.js
+++ client/src/Redux/Actions/actions.js
@@ -1,4 +1,4 @@
-import { GET_BREEDS, ERROR } from "./types"
+import { GET_BREEDS, ERROR, GET_BREEDS_BY_NAME } from "./types"
 import axios from 'axios'
 
 const HOST = "localhost";
@@ -11,6 +11,18 @@ export const getBreeds = () => {
             let breeds = await axios(`http://${HOST}:${PORT}/dogs`)
             console.log(breeds.data);
             return dispatch({ type: GET_BREEDS, payload: breeds.data })
+        } catch (error) {
+            return dispatch({ type: ERROR, payload: error })
+        }
+    }
+}
+
+export const getBreedsByName = (name) => {
+    return async function (dispatch) {
+        try {
+            let breeds = await axios(`http://${HOST}:${PORT}/dogs?name=${name}`)
+            if (!breeds.data?.msg) return dispatch({ type: GET_BREEDS_BY_NAME, payload: breeds.data })
+            return dispatch({ type: ERROR, payload: breeds.data.msg })
         } catch (error) {
             return dispatch({ type: ERROR, payload: error })
         }
--- client/src/Redux/Actions/types.js
+++ client/src/Redux/Actions/types.js
@@ -1,3 +1,4 @@
 export const GET_BREEDS = 'GET_BREEDS';
+export const GET_BREEDS_BY_NAME = 'GET_BREEDS_BY_NAME';
 
 export const ERROR = 'ERROR';
