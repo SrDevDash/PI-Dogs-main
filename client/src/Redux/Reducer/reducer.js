388f6d05614db6ea6db379cb8513dd8f78acc8c8
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-16 12:05:10 -0500

    Update reducer.js

--- client/src/Redux/Reducer/reducer.js
+++ client/src/Redux/Reducer/reducer.js
@@ -1,20 +1,32 @@
-import { GET_BREEDS } from "../Actions/types";
+import { GET_BREEDS, GET_BREEDS_BY_NAME, ERROR } from "../Actions/types";
 
 const initialState = {
-    allBreeds: []
+    allBreeds: [],
+    errors: ''
 };
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_BREEDS:
-
             return {
                 ...state,
                 allBreeds: action.payload
             }
 
+        case GET_BREEDS_BY_NAME:
+            return {
+                ...state,
+                allBreeds: action.payload
+            }
+
+        case ERROR:
+            console.log(action.payload);
+            return {
+                ...state,
+                errors: action.payload
+            }
         default:
             return {
-                ...state
+                ...state,
             }
     }
 }
