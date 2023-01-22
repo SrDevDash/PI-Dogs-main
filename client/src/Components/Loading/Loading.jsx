bc504dfab06e3eb8430b681252ea1cbbdff2c92d
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-22 13:35:36 -0500

    change loading gif

--- client/src/Components/Loading/Loading.jsx
+++ client/src/Components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className={style.loadingContainer}>
       <img
-        src="https://media.tenor.com/GPbjPPV9GrgAAAAM/oskar-boston-terrier.gif"
+        src="https://media0.giphy.com/media/Mvi1ZqG17NSso/giphy.gif?cid=6c09b952zip81pl7nmczzgs2edpne164cebaz3zpwdql72bv&rid=giphy.gif&ct=s"
         alt="loading"
       />
       <p>LOADING...</p>
