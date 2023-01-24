19f09ccca07efa6f98edd42c3176bbeb7b4f8790
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-24 16:27:32 -0500

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   PGUSER, PGHOST, PGPASSWORD, PGDATABASE, PGPORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
+const sequelize = new Sequelize(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}}/${PGDATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
