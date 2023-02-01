495cf23524d0e4708c5b5bc7be161047837600a6
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-01 09:56:50 -0500

    adding more filter

--- client/src/Components/CreateBreedForm/FormValidator.js
+++ client/src/Components/CreateBreedForm/FormValidator.js
@@ -5,12 +5,18 @@ export const validator = (data) => {
     else if (!/^[a-zA-Z ]+$/.test(data.name)) erros.name = 'Name should be a string'
 
     if (!data.minWeight) erros.weight = 'Weight is required';
+    else if (data.minWeight <= 0) erros.weight = 'Weight should be greater than 0';
+    else if (!data.maxWeight) erros.weight = 'Weight is required';
     else if (parseInt(data.minWeight) > parseInt(data.maxWeight)) erros.weight = "Min weight should be lower than Max weight"
 
-    if (!data.minHeight) erros.height = 'Height is required'
+    if (!data.minHeight) erros.height = 'Height is required';
+    else if (data.minHeight <= 0) erros.height = 'Height should be greater than 0';
+    else if (!data.maxHeight) erros.height = 'Weight is required';
     else if (parseInt(data.minHeight) > parseInt(data.maxHeight)) erros.height = "Min height should be lower than Max height"
 
     if (!data.minLifeSpan) erros.lifeSpan = 'Life span is required'
+    else if (data.minLifeSpan <= 0) erros.lifeSpan = 'Life span should be greater than 0';
+    else if (!data.maxLifeSpan) erros.lifeSpan = 'Weight is required';
     else if (parseInt(data.minLifeSpan) > parseInt(data.maxLifeSpan)) erros.lifeSpan = "Min life span should be lower than Max life span"
 
     if (!data.image) erros.image = 'Image is required'
